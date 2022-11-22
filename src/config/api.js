d811c3455475c61be718701326ca9d0126a2ca1b
Connor <10349@coderacademy.edu.au>
2022-11-22 12:47:52 +1000

    updated axios baseUrl from localhost to netlify app because merging this dev branch with main deployed branch.

--- src/config/api.js
+++ src/config/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const fitalyticsAPI = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://fitalytics-api.herokuapp.com'
 })
 
-// https://fitalytics-api.herokuapp.com
+// http://localhost:3000
 
 fitalyticsAPI.interceptors.request.use((req) => {
     const token = localStorage.getItem('token');
